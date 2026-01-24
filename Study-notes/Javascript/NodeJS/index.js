93ca5e5380b8afabbccbf021745f1629e412b8cd
abinayaa-dev <59796356+abinayaa-dev@users.noreply.github.com>
2026-01-24 17:08:29 +0000

    Blocking and Non-Blocking code

--- Study-notes/Javascript/NodeJS/index.js
+++ Study-notes/Javascript/NodeJS/index.js
@@ -1,5 +1,34 @@
+const { error } = require('console');
 const fs= require('fs'); // Built-in module
 
-const content = fs.readFileSync('notes.txt' , 'utf-8');
+// const content = fs.readFileSync('notes.txt' , 'utf-8');
 
+// console.log(content);
+fs.writeFileSync('copy.txt', 'hii','utf-8');
+
+fs.appendFileSync('copy.txt', '\n\nhello','utf-8');
+fs.mkdirSync('./file/copy1.txt',{recursive : true});
+
+//Blocking code
+
+console.log("Start of code");
+
+const content = fs.readFileSync("notes.txt","utf-8");
 console.log(content);
+
+
+console.log("End of code");
+
+//Non-Blocking code
+
+console.log("Start of code");
+
+fs.readFile("notes.txt",'utf-8',(error,data)=>{
+    if(error){console.log(error);
+    }
+    else{
+        console.log(data);
+    }
+});
+
+console.log("End of code");
